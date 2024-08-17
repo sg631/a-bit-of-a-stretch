eebcdc0bba3821563841eb350a6eb781f9fd44fe
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-08-17 11:04:43 -0700

    Added.. STRETCHY MAN!!!

--- playermove.gd
+++ playermove.gd
@@ -10,15 +10,46 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("stretch_down") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#Stretching Movement
+	if Input.is_action_pressed("stretch_right"):
+		print("lol");
+		scale.x += 0.2;
+		position.x += 2;
+	if Input.is_action_pressed("stretch_left"):
+		print("lol");
+		scale.x += 0.2;
+		position.x -= 2;
+	if Input.is_action_pressed("stretch_up"):
+		print("lol");
+		scale.y += 0.2;
+		position.y += 2;
+	if Input.is_action_pressed("stretch_down"):
+		print("lol");
+		scale.y += 0.2;
+		position.y += 2;
+	#Shrinking Movement
+	if Input.is_action_pressed("shrink_left"):
+		print(scale.x);
+		if scale.x >= 1:
+			scale.x -= 0.2;
+			position.x -= 2;
+	if Input.is_action_pressed("shrink_right"):
+		print(scale.x);
+		if scale.x >= 1:
+			scale.x -= 0.2;
+			position.x += 2;
+	if Input.is_action_pressed("shrink_up"):
+		print(scale.x);
+		if scale.y >= 1:
+			scale.y -= 0.2;
+			position.y -= 2;
+	if Input.is_action_pressed("shrink_down"):
+		print(scale.x);
+		if scale.y >= 1:
+			scale.y -= 0.2;
+			position.y += 2;
 
 	move_and_slide()
