a95b35dd09ba4bd83bdeb616f989b470bacf2544
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-08-18 21:56:01 -0700

    c

--- collect-orb.gd
+++ collect-orb.gd
@@ -1,8 +1,11 @@
 extends RigidBody2D
 
+var CorrectSound = preload("res://pickupCoin.wav")
+
 func _physics_process(delta: float) -> void:
 	for body in get_colliding_bodies():
 		if body.editor_description == "Player":
 			print("TOUCHING PLAYER")
 			get_parent().remove_child(get_parent().get_children()[0])
 			Autoload.growPoints += 1;
+	
