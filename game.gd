ed7dc3657f17f2860c04e69dea8f506633b0a23d
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-08-18 21:39:41 -0700

    Revert "game, man"
    This reverts commit e0a6e5b8efae38b9a28382f464481b4140e0f45e.

--- game.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-var CorrectSound = preload("res://backtrackz.wav")
-
-func _process(delta: float) -> void:
-	if !$AudioStreamPlayer.is_playing():
-		$AudioStreamPlayer.stream = CorrectSound
-		$AudioStreamPlayer.play()
