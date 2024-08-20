5038307dc9681a84253dc80b2fe4b7132a9b220e
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-08-19 21:38:21 -0700

    Fixed shop upgrades not working

--- efficiency_upgrade.gd
+++ efficiency_upgrade.gd
@@ -1,6 +1,6 @@
 extends Button
 
 func _pressed() -> void:
-	if Autoload.growPoints >= 2 and Autoload.upgradeEfficiency < 2:
+	if Autoload.growPoints >= 2 and Autoload.upgradeEfficiency > 0.001:
 		Autoload.upgradeEfficiency *= 0.9;
 		Autoload.growPoints -= 2;
--- length_upgrade.gd
+++ length_upgrade.gd
@@ -1,6 +1,6 @@
 extends Button
 
 func _pressed() -> void:
-	if Autoload.growPoints >= 3 and Autoload.upgradeLength < 2:
+	if Autoload.growPoints >= 3 and Autoload.upgradeLength < 8:
 		Autoload.upgradeLength += 1;
 		Autoload.growPoints -= 3;
