127c1f622901d5e5c63da03303353925268bafc4
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-08-25 16:59:00 -0700

    checkmate

--- game.gd
+++ game.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 var CorrectSound = preload("res://backtrackz.wav")
-
+func _ready() -> void:
+	get_node("/root/Game/Player").position = Vector2(Autoload.checkpoint["x"], Autoload.checkpoint["y"])
 func _process(delta: float) -> void:
+	print(Autoload.checkpoint)
 	if !$AudioStreamPlayer.is_playing():
 		$AudioStreamPlayer.stream = CorrectSound
 		$AudioStreamPlayer.play()
-		get_node("/root/Game/Player").position = Vector2(Autoload.checkpoint["x"], Autoload.checkpoint["y"])
+		
--- playermove.gd
+++ playermove.gd
@@ -4,15 +4,20 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 
+var is_just_spawned = true
 
+func _init() -> void:
+	position.x = Autoload.checkpoint["x"]
+	position.y = Autoload.checkpoint["y"]
 func _physics_process(delta: float) -> void:
+	position.x = Autoload.checkpoint["x"]
+	position.y = Autoload.checkpoint["y"]
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	# Handle jump.
 	#if Input.is_action_just_pressed("stretch_down") and is_on_floor():
 		#velocity.y = JUMP_VELOCITY
-	
 	#Stretching Movement
 	if Input.is_action_pressed("stretch_right"):
 		if scale.x < Autoload.upgradeLength:
@@ -97,5 +102,6 @@ func _physics_process(delta: float) -> void:
 				Autoload.growPoints += Autoload.upgradeEfficiency/4;
 	if Autoload.growPoints < 0:
 		get_parent().remove_child(get_parent().get_children()[0]);
+	
 
 	move_and_slide()
