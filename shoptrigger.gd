3ff4bd52c726ef0921c6c2252dce5b5bdd7d8d9e
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-08-24 22:15:14 -0700

    Fixed devastating shop lag (POST-JAM)
    Wow, the jam is over! Anyway,

--- shoptrigger.gd
+++ shoptrigger.gd
@@ -3,8 +3,9 @@ extends Area2D
 func _physics_process(delta: float) -> void:
 	for body in get_overlapping_bodies():
 		if body.editor_description == "Player":
-			if not get_parent().get_parent().has_node("ShopUI"):
+			if not Autoload.shopLoaded:
 				var scene = load("res://shop.tscn")
 				var instance = scene.instantiate();
 				var canvasLayer = get_parent().get_parent().get_node("HUD")
 				canvasLayer.add_child(instance)
+				Autoload.shopLoaded = true;
